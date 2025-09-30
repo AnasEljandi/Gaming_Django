ff80c88693554a2295640352ecd0a5406ad90633
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-30 17:10:21 +0100

    deploy the project

--- gamestar/settings.py
+++ gamestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#!7dyh#i5=cy!lb40$5a5!0_*@!vbr5_@+vq$tt6mux&bb*5j-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
